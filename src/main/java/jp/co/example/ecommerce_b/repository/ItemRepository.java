a19e5f829331cbd9b5f4eb020c4b911958742d65
kazuhiro119 <kazuhiro0119@icloud.com>
2023-06-20 16:06:11 +0900

    商品の全件検索をするメソッドの追加

--- src/main/java/jp/co/example/ecommerce_b/repository/ItemRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/ItemRepository.java
@@ -1,8 +1,16 @@
 package jp.co.example.ecommerce_b.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.example.domain.Employee;
+
+import jp.co.example.ecommerce_b.domain.Item;
 
 /**
  * 
@@ -17,4 +25,16 @@ public class ItemRepository {
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
+	private static final RowMapper<Item> ITEM_ROW_MAPPER = new BeanPropertyRowMapper<>(Item.class);
+
+	/**
+	 * 商品一覧を検索するメソッド.
+	 * 
+	 * @return 全商品一覧情報
+	 */
+	public List<Item> findAll() {
+		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items ORDER BY price_m;";
+		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
+		return itemList;
+	}
 }
