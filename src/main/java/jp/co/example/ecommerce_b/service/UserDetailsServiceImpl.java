f6dfcf79b923bda37b046e6375934dd3d93b6942
mh214 <orrtrrttr@gmail.com>
2023-06-20 16:30:58 +0900

    ログイン処理のためのサービスを作成

---
+++ src/main/java/jp/co/example/ecommerce_b/service/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package jp.co.example.ecommerce_b.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import jp.co.example.ecommerce_b.domain.LoginUser;
+import jp.co.example.ecommerce_b.domain.User;
+import jp.co.example.ecommerce_b.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = repository.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("そのEmailは登録されていません。");
+		}
+		Collection<GrantedAuthority> authorityList = new ArrayList<>();
+		authorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
+		return new LoginUser(user, authorityList);
+	}
+}
