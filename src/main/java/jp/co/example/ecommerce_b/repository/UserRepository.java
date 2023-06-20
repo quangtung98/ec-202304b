1a7c91605f60b11ec5cfab707b13edd167bbba04
mh214 <orrtrrttr@gmail.com>
2023-06-20 16:30:32 +0900

    UserRepositoryのfindByEmailメソッドの作成

--- src/main/java/jp/co/example/ecommerce_b/repository/UserRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/UserRepository.java
@@ -1,8 +1,16 @@
 package jp.co.example.ecommerce_b.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+
+import jp.co.example.ecommerce_b.domain.User;
 
 /**
  * 
@@ -14,7 +22,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
 
+	private static final RowMapper<User> USER_ROW_MAPPER = new BeanPropertyRowMapper<>(User.class);
+
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
+	public User findByEmail(String email) {
+		String sql = "";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("email", email);
+		List<User> userList = template.query(sql, param, USER_ROW_MAPPER);
+		if (userList.size() == 0) {
+			return null;
+		}
+
+		return userList.get(0);
+	}
 }
