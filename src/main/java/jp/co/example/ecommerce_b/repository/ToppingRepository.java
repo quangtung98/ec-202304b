70ee5d67abda5ae9868323ed9e9db3ec3bd52378
kazuhiro119 <kazuhiro0119@icloud.com>
2023-06-21 14:03:54 +0900

    toppingsテーブルで全件検索するメソッドの追加

--- src/main/java/jp/co/example/ecommerce_b/repository/ToppingRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/ToppingRepository.java
@@ -1,8 +1,14 @@
 package jp.co.example.ecommerce_b.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import jp.co.example.ecommerce_b.domain.Topping;
 
 /**
  * 
@@ -16,4 +22,17 @@ public class ToppingRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
+
+	private static final RowMapper<Topping> TOPPING_ROW_MAPPER = new BeanPropertyRowMapper<>(Topping.class);
+
+	/**
+	 * toppingsテーブルで全件検索するメソッド.
+	 * 
+	 * @return 全トッピング一覧
+	 */
+	public List<Topping> findAll() {
+		String sql = "";
+		List<Topping> toppingList = template.query(sql, TOPPING_ROW_MAPPER);
+		return toppingList;
+	}
 }
