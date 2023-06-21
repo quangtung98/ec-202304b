716666af873e0084e78807c17a25f2772dbe2483
mh214 <orrtrrttr@gmail.com>
2023-06-21 14:07:04 +0900

    ショッピングカートを操作するコントローラの枠を作成

---
+++ src/main/java/jp/co/example/ecommerce_b/controller/ShoppingCartController.java
@@ -0,0 +1,32 @@
+package jp.co.example.ecommerce_b.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ショッピングカート情報を操作するコントローラー.
+ * 
+ * @author mami.horioka
+ *
+ */
+@Controller
+@RequestMapping("/shoppingCart")
+public class ShoppingCartController {
+
+	@GetMapping("/show")
+	public String showShoppingCart(int userId, int status) {
+		return null;
+	}
+
+	@PostMapping("/insert")
+	public String InsertShoppingCart(InsertShoppingCartForm form) {
+		return null;
+	}
+
+	@PostMapping("/delete")
+	public String deleteShoppingCart(int orderItemId) {
+		return null;
+	}
+}
