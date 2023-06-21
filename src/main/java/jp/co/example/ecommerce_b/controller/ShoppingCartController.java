d09ca379b8de1fcdced384863940dbd85a8004d3
mh214 <orrtrrttr@gmail.com>
2023-06-21 14:38:57 +0900

    コミット漏れ！

--- src/main/java/jp/co/example/ecommerce_b/controller/ShoppingCartController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/ShoppingCartController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.example.ecommerce_b.form.InsertShoppingCartForm;
+
 /**
  * ショッピングカート情報を操作するコントローラー.
  * 
--- src/main/java/jp/co/example/ecommerce_b/repository/OrderToppingRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/OrderToppingRepository.java
@@ -1,8 +1,12 @@
 package jp.co.example.ecommerce_b.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+
+import jp.co.example.ecommerce_b.domain.OrderTopping;
 
 /**
  * 
@@ -16,4 +20,15 @@ public class OrderToppingRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * 注文トッピングを挿入する.
+	 * 
+	 * @param orderTopping 注文トッピング
+	 */
+	public void insert(OrderTopping orderTopping) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(orderTopping);
+		String sql = "INSERT INTO order_toppings(topping_id, order_item_id) VALUES(toppingId, orderItemId);";
+	}
+
 }
