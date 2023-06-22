0346b23218a480353f2549f9c502e2ff0f78c287
mh214 <orrtrrttr@gmail.com>
2023-06-22 15:02:33 +0900

    ショッピングカートに商品を追加する機能、一部を除いて完成

--- src/main/java/jp/co/example/ecommerce_b/controller/ShoppingCartController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/ShoppingCartController.java
@@ -1,12 +1,14 @@
 package jp.co.example.ecommerce_b.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.example.ecommerce_b.domain.LoginUser;
 import jp.co.example.ecommerce_b.domain.Order;
 import jp.co.example.ecommerce_b.form.InsertShoppingCartForm;
 import jp.co.example.ecommerce_b.service.ShoppingCartService;
@@ -32,8 +34,10 @@ public class ShoppingCartController {
 	}
 
 	@PostMapping("/insert")
-	public String InsertShoppingCart(InsertShoppingCartForm form) {
-		return null;
+	public String InsertShoppingCart(InsertShoppingCartForm form, @AuthenticationPrincipal LoginUser loginUser,
+			Model model) {
+		service.insertShoppingCart(form, loginUser.getUser().getId());
+		return showShoppingCart(loginUser.getUser().getId(), 0, model);
 	}
 
 	@PostMapping("/delete")
--- src/main/java/jp/co/example/ecommerce_b/service/ShoppingCartService.java
+++ src/main/java/jp/co/example/ecommerce_b/service/ShoppingCartService.java
@@ -1,14 +1,22 @@
 package jp.co.example.ecommerce_b.service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.example.ecommerce_b.domain.Order;
+import jp.co.example.ecommerce_b.domain.OrderItem;
+import jp.co.example.ecommerce_b.domain.OrderTopping;
 import jp.co.example.ecommerce_b.form.InsertShoppingCartForm;
+import jp.co.example.ecommerce_b.repository.ItemRepository;
 import jp.co.example.ecommerce_b.repository.OrderItemRepository;
 import jp.co.example.ecommerce_b.repository.OrderRepository;
 import jp.co.example.ecommerce_b.repository.OrderToppingRepository;
+import jp.co.example.ecommerce_b.repository.ToppingRepository;
 
 /**
  * ショッピングカートを操作するサービス.
@@ -25,6 +33,10 @@ public class ShoppingCartService {
 	private OrderToppingRepository orderToppingRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private ToppingRepository toppingRepository;
+	@Autowired
+	private ItemRepository itemRepository;
 
 	/**
 	 * ショッピングカートを表示するサービス.
@@ -44,8 +56,42 @@ public class ShoppingCartService {
 	 * @param form フォーム
 	 * @return 注文
 	 */
-	public Order insertShoppingCart(InsertShoppingCartForm form) {
-		// TODO
+	public Order insertShoppingCart(InsertShoppingCartForm form, int userId) {
+
+		List<OrderTopping> orderToppingList = new ArrayList<>();
+		for (Integer orderToppingId : form.getOrderToppingList()) {
+			OrderTopping orderTopping = new OrderTopping();
+			orderTopping.setToppingId(orderToppingId);
+			orderTopping.setTopping(toppingRepository.load(orderToppingId));
+			orderToppingList.add(orderTopping);
+		}
+
+		OrderItem orderItem = new OrderItem();
+		orderItem.setItemId(form.getItemId());
+		orderItem.setSize(form.getSize().toCharArray()[0]);
+		orderItem.setQuantity(form.getQuantity());
+		orderItem.setOrderToppingList(orderToppingList);
+		orderItem.setItem(itemRepository.load(form.getItemId()));
+
+		Order myOrder = orderRepository.findByUserIdAndStatus(userId, 0);
+		if (myOrder.getId() == null) {
+			myOrder.setUserId(userId);
+			myOrder.setStatus(0);
+			myOrder.setOrderDate(new Date());
+			// 住所を追加する
+			myOrder.setTotalPrice(myOrder.getCalcTotalPrice());
+			myOrder = orderRepository.insert(myOrder);
+		} else {
+			// update!! 値段だけ？
+		}
+		orderItem.setOrderId(myOrder.getId());
+		orderItem = orderItemRepository.insert(orderItem);
+
+		for (OrderTopping orderTopping : orderToppingList) {
+			orderTopping.setOrderItemId(orderItem.getId());
+			orderToppingRepository.insert(orderTopping);
+		}
+
 		return null;
 	}
 
