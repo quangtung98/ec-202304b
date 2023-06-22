58d87e0d38f7721a10df554e568ddaa782696037
mh214 <orrtrrttr@gmail.com>
2023-06-22 15:02:08 +0900

    メソッド追加

--- src/main/java/jp/co/example/ecommerce_b/service/ShowItemDetailService.java
+++ src/main/java/jp/co/example/ecommerce_b/service/ShowItemDetailService.java
@@ -1,10 +1,15 @@
 package jp.co.example.ecommerce_b.service;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.example.ecommerce_b.domain.Item;
+import jp.co.example.ecommerce_b.domain.Topping;
 import jp.co.example.ecommerce_b.repository.ItemRepository;
 import jp.co.example.ecommerce_b.repository.ToppingRepository;
 
@@ -34,6 +39,15 @@ public class ShowItemDetailService {
 		Item item = itemRepository.load(id);
 		item.setToppingList(toppingRepository.findAll());
 		return item;
+	}
+
+	public Map<Integer, String> showToppings() {
+		List<Topping> toppingList = toppingRepository.findAll();
+		Map<Integer, String> toppingMap = new LinkedHashMap<>();
+		for (Topping topping : toppingList) {
+			toppingMap.put(topping.getId(), topping.getName());
+		}
+		return toppingMap;
 	}
 
 }
