c0f603868c938482c772a54bf89c01ccc4b556ee
kne310 <okanae310@gmail.com>
2023-06-22 10:28:40 +0900

    注文する業務処理を管理するサービスの作成

---
+++ src/main/java/jp/co/example/ecommerce_b/service/OrderService.java
@@ -0,0 +1,39 @@
+package jp.co.example.ecommerce_b.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.example.ecommerce_b.domain.Order;
+import jp.co.example.ecommerce_b.repository.OrderRepository;
+
+/**
+ * 注文する業務処理を操作するサービス.
+ * 
+ * @author kanae.osaki
+ *
+ */
+@Service
+@Transactional
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	/**
+	 * 注文情報を更新する.
+	 * 
+	 * @param form フォームより送られてきた注文情報
+	 */
+	public void order(OrderForm form) {
+		Order order = orderRepository.load(form.getOrderId());
+		BeanUtils.copyProperties(form, order);
+		if (form.getPaymentMethod() == 0) {
+			order.setStatus(1);
+		} else {
+			order.setStatus(2);
+		}
+		orderRepository.update(order);
+	}
+}
