b3149f0d3b0fa86228b36274951fe32f6de654a7
mh214 <orrtrrttr@gmail.com>
2023-06-23 14:54:21 +0900

    大変ありがとうかなかな

--- src/main/java/jp/co/example/ecommerce_b/repository/OrderRepository.java
+++ src/main/java/jp/co/example/ecommerce_b/repository/OrderRepository.java
@@ -92,6 +92,78 @@ public class OrderRepository {
 
 		return order;
 	};
+
+	/**
+	 * ordersとorder_itemsとorder_toppingsを結合したテーブルから取得したデータをList<Order>型のオブジェクトにセットするための定数
+	 * order_itemsはitemsと、order_toppingsはtoppingsと結合されている
+	 */
+	private static final ResultSetExtractor<List<Order>> ORDER_LIST_ITEM_TOPPING_RESULTSET = (rs) -> {
+		List<Order> orderList = new ArrayList<>();
+		List<OrderItem> orderItemList = null;
+		List<OrderTopping> orderToppingList = null;
+		int beforeItemIdNum = 0;
+		int beforeIdNum = 0;
+		while (rs.next()) {
+			int nowIdNum = rs.getInt("id");
+			if (beforeIdNum != nowIdNum) {
+				beforeIdNum = nowIdNum;
+				Order order = new Order();
+				orderItemList = new ArrayList<>();
+				order.setOrderItemList(orderItemList);
+
+				order.setId(rs.getInt("id"));
+				order.setUserId(rs.getInt("user_id"));
+				order.setStatus(rs.getInt("status"));
+				order.setTotalPrice(rs.getInt("total_price"));
+				order.setOrderDate(rs.getDate("order_date"));
+				order.setDestinationName(rs.getString("destination_name"));
+				order.setDestinationEmail(rs.getString("destination_email"));
+				order.setDestinationZipcode(rs.getString("destination_zipcode"));
+				order.setDestinationAddress(rs.getString("destination_address"));
+				order.setDestinationTel(rs.getString("destination_tel"));
+				order.setDeliveryTime(rs.getTimestamp("delivery_time"));
+				order.setPaymentMethod(rs.getInt("payment_method"));
+				orderList.add(order);
+			}
+			int nowItemIdNum = rs.getInt("order_item_id");
+			if (nowItemIdNum != beforeItemIdNum) {
+				beforeItemIdNum = nowItemIdNum;
+				OrderItem orderItem = new OrderItem();
+				orderItem.setId(nowItemIdNum);
+				orderItem.setItemId(rs.getInt("item_id"));
+				orderItem.setQuantity(rs.getInt("quantity"));
+				orderItem.setSize(rs.getString("size").charAt(0));
+				Item item = new Item();
+				item.setId(rs.getInt("item_id"));
+				item.setName(rs.getString("item_name"));
+				item.setDescription(rs.getString("description"));
+				item.setPriceM(rs.getInt("item_price_m"));
+				item.setPriceL(rs.getInt("item_price_l"));
+				item.setImagePath(rs.getString("image_path"));
+				orderItem.setItem(item);
+				orderToppingList = new ArrayList<OrderTopping>();
+				orderItem.setOrderToppingList(orderToppingList);
+
+				orderItemList.add(orderItem);
+			}
+			if (rs.getInt("order_topping_id") != 0) {
+				OrderTopping orderTopping = new OrderTopping();
+				orderTopping.setId(rs.getInt("order_topping_id"));
+				orderTopping.setToppingId(rs.getInt("topping_id"));
+				Topping topping = new Topping();
+				topping.setId(rs.getInt("topping_id"));
+				topping.setName(rs.getString("topping_name"));
+				topping.setPriceM(rs.getInt("topping_price_m"));
+				topping.setPriceL(rs.getInt("topping_price_l"));
+				orderTopping.setTopping(topping);
+				orderToppingList.add(orderTopping);
+			}
+
+		}
+
+		return orderList;
+
+	};
 	private static final String ORDER_SELECT_SQL = "SELECT a.id , a.user_id , a.status , a.total_price , a.order_date , a.destination_name , a.destination_email , a.destination_zipcode , destination_pref , destination_municipalities , a.destination_address , a.destination_tel , a.delivery_time , a.payment_method , b.id AS order_item_id , b.item_id , b.quantity , b.size , b.name AS item_name , b.description , b.price_m AS item_price_m , b.price_l AS item_price_l , b.image_path , b.deleted , c.id AS order_topping_id , c.topping_id , c.name AS topping_name , c.price_m AS topping_price_m , c.price_l AS topping_price_l FROM orders AS a LEFT OUTER JOIN (SELECT d.id , item_id , quantity , order_id , size , name , description , price_m , price_l , image_path , deleted FROM order_items AS d LEFT OUTER JOIN items ON d.item_id = items.id) AS b ON a.id = b.order_id LEFT OUTER JOIN (SELECT e.id ,order_item_id ,topping_id , name , price_m , price_l FROM order_toppings AS e LEFT OUTER JOIN toppings ON e.topping_id = toppings.id) AS c ON b.id = c.order_item_id ";
 
 	/**
@@ -148,5 +220,18 @@ public class OrderRepository {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
 		String updateSql = "UPDATE orders SET status = :status,total_price=:totalPrice,order_date=:orderDate,destination_name=:destinationName,destination_email=:destinationEmail,destination_zipcode=:destinationZipcode,destination_pref=:destinationPref,destination_municipalities=:destinationMunicipalities,destination_address=:destinationAddress,destination_tel=:destinationTel,delivery_time=:deliveryTime,payment_method=:paymentMethod WHERE id=:id;";
 		template.update(updateSql, param);
+	}
+
+	/**
+	 * ユーザーIDで絞り込んで注文のリストを取得する.
+	 * 
+	 * @param userId ユーザーID
+	 * @return 注文のリスト
+	 */
+	public List<Order> findAllByUserId(int userId) {
+		String sql = ORDER_SELECT_SQL + "WHERE a.user_id =:userId ORDER BY order_item_id;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("userId", userId);
+		List<Order> orderList = template.query(sql, param, ORDER_LIST_ITEM_TOPPING_RESULTSET);
+		return orderList;
 	}
 }
