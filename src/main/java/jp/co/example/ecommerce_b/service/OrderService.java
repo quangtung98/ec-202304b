6f61a8088670edec871436a838ffac05d5b626cd
kne310 <okanae310@gmail.com>
2023-06-23 11:38:08 +0900

    メール送信機能の作成

--- src/main/java/jp/co/example/ecommerce_b/service/OrderService.java
+++ src/main/java/jp/co/example/ecommerce_b/service/OrderService.java
@@ -7,9 +7,13 @@ import java.time.LocalTime;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jp.co.example.ecommerce_b.domain.Order;
 import jp.co.example.ecommerce_b.form.OrderForm;
 import jp.co.example.ecommerce_b.repository.OrderRepository;
@@ -26,6 +30,9 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private JavaMailSender sender;
 
 	/**
 	 * 注文情報を更新する.
@@ -45,5 +52,19 @@ public class OrderService {
 			order.setStatus(2);
 		}
 		orderRepository.update(order);
+		MimeMessage message = sender.createMimeMessage();
+	    try {
+	      //送信情報設定
+	      MimeMessageHelper helper = new MimeMessageHelper(message, true);
+	      helper.setFrom("okanae310@gmail.com");
+	      helper.setTo(order.getDestinationEmail());
+	      helper.setSubject("注文完了メール");
+	      helper.setText("ご注文を承りました。"+order.getDeliveryTime().toString()+"にお届け予定です！");
+	      
+	      //メール送信
+	      sender.send(message);
+	    } catch(MessagingException e) {
+	      e.printStackTrace();
+	    }
 	}
 }
