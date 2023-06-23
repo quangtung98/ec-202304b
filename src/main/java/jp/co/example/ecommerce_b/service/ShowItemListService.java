d10fc69463e87435a2776d7d88ec1f1744dc69ec
kazuhiro119 <kazuhiro0119@icloud.com>
2023-06-23 11:31:05 +0900

    ソートされたそれぞれの商品一覧を取得するメソッドを追加しました

--- src/main/java/jp/co/example/ecommerce_b/service/ShowItemListService.java
+++ src/main/java/jp/co/example/ecommerce_b/service/ShowItemListService.java
@@ -26,15 +26,68 @@ public class ShowItemListService {
 	/**
 	 * 表示する商品の検索を行うメソッド（初回ログイン時はnullのため全件検索される）
 	 * 
-	 * @param fuzzyName 入力された名前（あいまい検索に使用）
+	 * @param fuzzyName  入力された名前（あいまい検索に使用）
+	 * @param sortMethod ソート方法
 	 * @return 表示する商品一覧
 	 */
-	public List<Item> showItemList(String fuzzyName) {
+	public List<Item> showItemList(String fuzzyName, String sortMethod) {
 		List<Item> itemList = new ArrayList<>();
 		if (fuzzyName == null) {
-			itemList = itemRepository.findAll();
+			itemList = getSortedFindAll(sortMethod);
 		} else {
-			itemList = itemRepository.findByNameContaining(fuzzyName);
+			itemList = getSortedFindByNameContaining(fuzzyName, sortMethod);
+		}
+		return itemList;
+	}
+
+	/**
+	 * ソートされた全件検索を行うメソッド.
+	 * 
+	 * @param sortMethod ソート方法
+	 * @return ソートされた全商品一覧
+	 */
+	private List<Item> getSortedFindAll(String sortMethod) {
+		List<Item> itemList = new ArrayList<>();
+		if (sortMethod == null) {
+			itemList = itemRepository.findAll("name", "ASC");
+		} else if (sortMethod.equals("名前の昇順")) {
+			itemList = itemRepository.findAll("name", "ASC");
+		} else if (sortMethod.equals("名前の降順")) {
+			itemList = itemRepository.findAll("name", "DESC");
+		} else if (sortMethod.equals("Mサイズ価格の昇順")) {
+			itemList = itemRepository.findAll("price_m", "ASC");
+		} else if (sortMethod.equals("Mサイズ価格の降順")) {
+			itemList = itemRepository.findAll("price_m", "DESC");
+		} else if (sortMethod.equals("Lサイズ価格の昇順")) {
+			itemList = itemRepository.findAll("price_l", "ASC");
+		} else if (sortMethod.equals("Lサイズ価格の降順")) {
+			itemList = itemRepository.findAll("price_l", "DESC");
+		}
+		return itemList;
+	}
+
+	/**
+	 * ソートされた曖昧検索を行うメソッド.
+	 * 
+	 * @param sortMethod ソート方法
+	 * @return ソートされたあいまい検索をした全商品一覧
+	 */
+	private List<Item> getSortedFindByNameContaining(String fuzzyName, String sortMethod) {
+		List<Item> itemList = new ArrayList<>();
+		if (sortMethod == null) {
+			itemList = itemRepository.findByNameContaining(fuzzyName,"name", "ASC");
+		} else if (sortMethod.equals("名前の昇順")) {
+			itemList = itemRepository.findByNameContaining(fuzzyName,"name", "ASC");
+		} else if (sortMethod.equals("名前の降順")) {
+			itemList = itemRepository.findByNameContaining(fuzzyName,"name", "DESC");
+		} else if (sortMethod.equals("Mサイズ価格の昇順")) {
+			itemList = itemRepository.findByNameContaining(fuzzyName,"price_m", "ASC");
+		} else if (sortMethod.equals("Mサイズ価格の降順")) {
+			itemList = itemRepository.findByNameContaining(fuzzyName,"price_m", "DESC");
+		} else if (sortMethod.equals("Lサイズ価格の昇順")) {
+			itemList = itemRepository.findByNameContaining(fuzzyName,"price_l", "ASC");
+		} else if (sortMethod.equals("Lサイズ価格の降順")) {
+			itemList = itemRepository.findByNameContaining(fuzzyName,"price_l", "DESC");
 		}
 		return itemList;
 	}
