ae8caa959e5504ce2259d3fa0f117fb296013dc0
kazuhiro119 <kazuhiro0119@icloud.com>
2023-06-26 14:01:11 +0900

    カード情報記入欄の作成

--- src/main/java/jp/co/example/ecommerce_b/controller/ShowOrderConfirmController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/ShowOrderConfirmController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jp.co.example.ecommerce_b.form.CreditCardForm;
 import jp.co.example.ecommerce_b.form.OrderForm;
 import jp.co.example.ecommerce_b.service.ShowOrderConfirmService;
 
