fec50a1b3ef3539116f46d4f4bf89e62979d81a6
kazuhiro119 <kazuhiro0119@icloud.com>
2023-06-26 11:16:50 +0900

    フォームを使うために表示するとこの引数に追加

--- src/main/java/jp/co/example/ecommerce_b/controller/ShowOrderConfirmController.java
+++ src/main/java/jp/co/example/ecommerce_b/controller/ShowOrderConfirmController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.example.ecommerce_b.form.CreditCardForm;
 import jp.co.example.ecommerce_b.form.OrderForm;
 import jp.co.example.ecommerce_b.service.ShowOrderConfirmService;
 
@@ -32,14 +33,14 @@ public class ShowOrderConfirmController {
 	 * @return 注文確認画面
 	 */
 	@PostMapping("/")
-	public String showOrderConfirm(Integer orderId, Model model,OrderForm form) {
-		if(form.getDeliveryDate()==null) {
-			form.setDeliveryDate(new Date(System.currentTimeMillis()));			
+	public String showOrderConfirm(Integer orderId, Model model, OrderForm form, CreditCardForm creditCardForm) {
+		if (form.getDeliveryDate() == null) {
+			form.setDeliveryDate(new Date(System.currentTimeMillis()));
 		}
-		if(form.getDeliveryTime()==0) {
+		if (form.getDeliveryTime() == 0) {
 			form.setDeliveryTime(10);
 		}
-		if(form.getPaymentMethod()==null) {
+		if (form.getPaymentMethod() == null) {
 			form.setPaymentMethod(1);
 		}
 		model.addAttribute("order", showOrderConfirmService.showOrderConfirm(orderId));
